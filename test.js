8a2b1c3dfdf5c2db9f8c46991ccadb5cc12ca6a8
E.Azer Koçulu <azer@kodfabrik.com>
2014-09-17 23:53:37 -0700

    update tests

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var virtual = require('./');
 var test = require("prova");
 
-var simple = '<div class="foo bar" style="color: red; background: yellow; background-image: url(http://f-oo.com/bar.jpg?qux=corge&span=eggs);">yo</div>';
+var simple = '<div class="foo bar" style="color: red; background: yellow; background-image: url(http://f-oo.com/bar.jpg?qux=corge&span=eggs);" data-foo="bar">yo</div>';
 
 test('creating virtual dom from HTML', function (t) {
-  t.plan(5);
+  t.plan(6);
 
   virtual(simple, function (error, dom) {
     if (error) t.error(error);
@@ -13,5 +13,6 @@ test('creating virtual dom from HTML', function (t) {
     t.equal(dom.properties.style.color, 'red');
     t.equal(dom.properties.style.background, 'yellow');
     t.equal(dom.properties.style['background-image'], 'url(http://f-oo.com/bar.jpg?qux=corge&span=eggs)');
+    t.equal(dom.properties['data-foo'], 'bar');
   });
 });

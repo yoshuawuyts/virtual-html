26fa41e0f2925d3cc57be3d21f62839ccabc2580
James Halliday <mail@substack.net>
2015-03-15 15:37:42 -0700

    case insensitive tagName comparison

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ test('creating virtual dom from HTML', function (t) {
 
   virtual(simple, function (error, dom) {
     if (error) t.error(error);
-    t.equal(dom.tagName, 'div');
+    t.equal(dom.tagName.toLowerCase(), 'div');
     t.equal(dom.children[0].text, 'yo');
     t.equal(dom.properties.style.color, 'red');
     t.equal(dom.properties.style.background, 'yellow');
